c0d11e5aa2ce17ca8bf2b4438c33cd7b7f81868d
laurakelly1 <lkelly.laura@gmail.com>
2022-09-08 16:09:03 -0400

    Implemented fade in from below on home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./styles.scss";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-
+ 
 function App() {
   return (
     <div className="App">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,26 @@
 import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
+function FadeInSection(props) {
+  const [isVisible, setVisible] = useState(true);
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
 
 const Home = () => {
   return (
@@ -46,103 +68,109 @@ const Home = () => {
         </div>
       </div>
       <div className="about-page" id="about">
-        <h2 className="about-page-heading">Based in Norfolk, Virginia</h2>
-        <div className="profile-content">
-          <div className="profile-image"></div>
-          <div className="profile-text">
-            <h3 className="about-page-subheading">
-              I'm a full-stack developer with a deep appreciation for the design
-              process.
-            </h3>
-            <p>
-              With formal training in Pure Mathematics and two years of
-              publishing textbooks for Oxford University Press, I understand
-              what it takes for cross-functional teams to be successful.
-              <br />
-              <br />
-              My love for mathematics and problem solving drew me to a career in
-              software development.
-              <br />
-              <br />
-              When I'm not coding, you can find me hiking up mountains or
-              sipping cappuccinos in my local coffee house.
-            </p>
+        <FadeInSection>
+          <h2 className="about-page-heading">Based in Norfolk, Virginia</h2>
+          <div className="profile-content">
+            <div className="profile-image"></div>
+            <div className="profile-text">
+              <h3 className="about-page-subheading">
+                I'm a full-stack developer with a deep appreciation for the
+                design process.
+              </h3>
+              <p>
+                With formal training in Pure Mathematics and two years of
+                publishing textbooks for Oxford University Press, I understand
+                what it takes for cross-functional teams to be successful.
+                <br />
+                <br />
+                My love for mathematics and problem solving drew me to a career
+                in software development.
+                <br />
+                <br />
+                When I'm not coding, you can find me hiking up mountains or
+                sipping cappuccinos in my local coffee house.
+              </p>
+            </div>
           </div>
-        </div>
+        </FadeInSection>
       </div>
       <div className="projects-page" id="projects">
-        <h2 className="page-heading">Projects</h2>
-        <div className="project-tiles">
-          <a href="/projectforward">
-            <div className="project-tile" id="projectforward"></div>
-          </a>
-          <a href="/applied">
-            <div className="project-tile" id="project-1"></div>
-          </a>
-          <a href="/flowerbox">
-            <div className="project-tile" id="project-2"></div>
-          </a>
-          <a href="/plantanullius">
-            <div className="project-tile" id="project-3"></div>
-          </a>
-          <a href="/stillwall">
-            <div className="project-tile" id="project-4"></div>
-          </a>
-        </div>
+        <FadeInSection>
+          <h2 className="page-heading">Projects</h2>
+          <div className="project-tiles">
+            <a href="/projectforward">
+              <div className="project-tile" id="projectforward"></div>
+            </a>
+            <a href="/applied">
+              <div className="project-tile" id="project-1"></div>
+            </a>
+            <a href="/flowerbox">
+              <div className="project-tile" id="project-2"></div>
+            </a>
+            <a href="/plantanullius">
+              <div className="project-tile" id="project-3"></div>
+            </a>
+            <a href="/stillwall">
+              <div className="project-tile" id="project-4"></div>
+            </a>
+          </div>
+        </FadeInSection>
       </div>
       <div className="skills-page" id="skills">
-        <div className="skill-total-block">
-          <h2 className="skills-page-heading">Skills</h2>
-          <hr />
-          <div className="skill-lists">
-            <div className="skills-block">
-              <h3>Languages</h3>
-              <p>JavaScript</p>
-              <p>HTML5</p>
-              <p>CSS3</p>
-              <p>ES6</p>
-              <p>Python</p>
-            </div>
-            <div className="skills-block">
-              <h3>Frameworks</h3>
-              <p>React JS</p>
-              <p>Express JS</p>
-              <p>Node JS</p>
-              <p>django</p>
-            </div>
-            <div className="skills-block">
-              <h3>Libraries</h3>
-              <p>jQuery</p>
-              <p>Material UI</p>
-              <p>Materialize</p>
-              <p>Bootstrap</p>
-              <p>SASS</p>
-            </div>
-            <div className="skills-block">
-              <h3>Databases</h3>
-              <p>MongoDB</p>
-              <p>Mongoose</p>
-              <p>PostgreSQL</p>
-              <p>AWS S3</p>
-              <p>Google Firebase</p>
-            </div>
-            <div className="skills-block">
-              <h3>Deployment</h3>
-              <p>Heroku</p>
-              <p>Netlify</p>
-              <p>GitHub Pages</p>
-            </div>
-            <div className="skills-block">
-              <h3>Version Control</h3>
-              <p>Git</p>
-              <p>GitHub</p>
+        <FadeInSection>
+          <div className="skill-total-block">
+            <h2 className="skills-page-heading">Skills</h2>
+            <hr />
+            <div className="skill-lists">
+              <div className="skills-block">
+                <h3>Languages</h3>
+                <p>JavaScript</p>
+                <p>HTML5</p>
+                <p>CSS3</p>
+                <p>ES6</p>
+                <p>Python</p>
+              </div>
+              <div className="skills-block">
+                <h3>Frameworks</h3>
+                <p>React JS</p>
+                <p>Express JS</p>
+                <p>Node JS</p>
+                <p>Angular JS</p>
+                <p>django</p>
+              </div>
+              <div className="skills-block">
+                <h3>Libraries</h3>
+                <p>jQuery</p>
+                <p>Material UI</p>
+                <p>Materialize</p>
+                <p>Bootstrap</p>
+                <p>SASS</p>
+              </div>
+              <div className="skills-block">
+                <h3>Databases</h3>
+                <p>MongoDB</p>
+                <p>Mongoose</p>
+                <p>PostgreSQL</p>
+                <p>AWS S3</p>
+                <p>Google Firebase</p>
+              </div>
+              <div className="skills-block">
+                <h3>Deployment</h3>
+                <p>Heroku</p>
+                <p>Netlify</p>
+                <p>GitHub Pages</p>
+              </div>
+              <div className="skills-block">
+                <h3>Version Control</h3>
+                <p>Git</p>
+                <p>GitHub</p>
+              </div>
             </div>
           </div>
-        </div>
+        </FadeInSection>
       </div>
     </motion.div>
   );
 };
 
 export default Home;
-
